385246ff5adc056b50f707a4b1f033dee57b0ee1
Ruben Vandeginste <ruben_vandeginste@peopleware.be>
2026-06-30 10:55:40 +0200

    ExceptionHandlers: do not log intended behavior as error
    Exception handlers:
    - BadRequestException: log as information
    - OperationCancelledException: log as information

--- src/PPWCode.Server.Core/API/Exceptions/BadRequestExceptionHandler.cs
+++ src/PPWCode.Server.Core/API/Exceptions/BadRequestExceptionHandler.cs
@@ -24,7 +24,7 @@ namespace PPWCode.Server.Core.API.Exceptions
         {
             if (context.Exception is BadRequestException e)
             {
-                Logger.LogError(e.Message, e);
+                Logger.LogInformation(e.Message, e);
                 context.Result = new BadRequestObjectResult(e.Message);
                 return true;
             }
